952715ee4ebb09c0b56129dcb20e79230c431253
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-02-19 18:51:23 +0100

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorButton = document.querySelector(".widget button");
+const body = document.querySelector("body");
+const bgColorText = document.querySelector(".color");
+
+colorButton.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  bgColorText.textContent = getRandomHexColor();
+  // console.log(getRandomHexColor());
+  // console.log(body);
+});
