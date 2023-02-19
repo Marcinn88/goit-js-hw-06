c4eb86dc12cf024c7c5bf7d01f141e1ae72f2ae4
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-02-19 18:39:38 +0100

    8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", loginForm);
+
+function loginForm(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = form;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Przed wysłaniem proszę uzupełnić wszystkie pola formularza!");
+  }
+
+  const data = { email: email.value, password: password.value };
+  console.log(data);
+  form.reset();
+}
